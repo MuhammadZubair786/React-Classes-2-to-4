61b5dad6a27899b9d9ad604f2a5e2a63d935f603
MuhammadZubair786 <mzhassan444@gmail.com>
2021-10-10 14:18:40 +0500

    first class

---
+++ src/Components/Hello.js
@@ -0,0 +1,9 @@
+function Hello() {
+    return (
+      <div >
+      <h1>Hello Class</h1>
+      </div>
+    );
+  }
+  
+export default Hello;
---
+++ src/Components/Name3.js
@@ -0,0 +1,17 @@
+function Name() {
+    return (
+      <div >
+      <h2>Muhammad Zubair</h2>
+      </div>
+    );
+  }
+
+  function Name2() {
+    return (
+      <div >
+      <h2>Muhammad </h2>
+      </div>
+    );
+  }
+  
+export { Name2,Name}
