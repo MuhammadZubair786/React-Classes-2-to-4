d13da38a06818a6cb793b6763040df2cfd70ba49
saylani-massit-hyderbad <saylanimasshyderabad@gmail.com>
2021-12-22 12:43:51 +0500

    Class_10

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import LineChart from './Charts/line';
+
+function App() {
+  return (
+    <LineChart />
+   
+  );
+}
+
+export default App;
---
+++ src/Charts/line.js
@@ -0,0 +1,13 @@
+import {Line} from 'react-chartjs-2'
+
+function LineChart(){
+ 
+    return(
+        <>
+        <h1>Line Chart</h1>
+      
+        </>
+    )
+}
+
+export default LineChart;
